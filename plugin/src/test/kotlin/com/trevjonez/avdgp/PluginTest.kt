6d7c97ae92c2dc8cdd77df7c9ac69cfff54f6405
TrevJonez <trevorjones141@gmail.com>
2017-10-11 19:16:40 -0600

    simple file dsl methods to make test setup more declarative make gradle task to unzip sdk since it is good at that sort of thing.

--- plugin/src/test/kotlin/com/trevjonez/avdgp/PluginTest.kt
+++ plugin/src/test/kotlin/com/trevjonez/avdgp/PluginTest.kt
@@ -19,28 +19,47 @@ package com.trevjonez.avdgp
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 import java.io.File
 
 @RunWith(JUnit4::class)
 class PluginTest {
-    @get:Rule val testProjectDir = BuildDirFolder("PluginTest")
+    @get:Rule
+    val testDir = BuildDirFolder("PluginTest")
 
     @Test
     fun testForBasicTaskCreation() {
 
-        testProjectDir.apply {
-            File(file, "local.properties").writeText("sdk.dir=${System.getenv("ANDROID_HOME")}", Charsets.UTF_8)
+        var projectDir: File? = null
+        var sdkDir: File? = null
+        testDir.file.apply {
+            childDirectory("Android") {
+                sdkDir = childDirectory("sdk") {
+                    File(System.getProperty("sdkToolsPath"))
+                            .copyRecursively(childFile("tools"))
+                }
+            }
+
+            projectDir = childDirectory("sampleProject") {
+                childFile("local.properties").writeText("sdk.dir=${sdkDir?.absolutePath}")
+            }
         }
 
         GradleRunner.create()
-                .withGradleVersion("4.1")
-                .withProjectDir(testProjectDir.root)
+                .withProjectDir(projectDir)
                 .withArguments("tasks", "--stacktrace")
                 .withDebug(true)
                 .forwardOutput()
                 .build()
     }
+
+    private fun File.childDirectory(dirName: String, block: File.() -> Unit = {}): File {
+        return File(this, dirName).apply {
+            mkdir()
+            block()
+        }
+    }
+
+    private fun File.childFile(name: String) = File(this, name)
 }
